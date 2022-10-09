d9e3a293fb90f8dd837b66ebdbc262b42c593d53
sarabarslund <sara.barslund@outlook.com>
2022-10-09 18:08:59 +0200

    Dice feature

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Indsætter værdier
+        int dice1, dice2;
+
+        dice1 = new Random().nextInt(1, 7);
+        dice2 = new Random().nextInt(1, 7);
+
+        System.out.println("Dice 1 roll: " + dice1 + ", dice 2 roll: " + dice2 + ".");
     }
 }
