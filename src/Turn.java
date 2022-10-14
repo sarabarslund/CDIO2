8226288d78cae755db82780cba25cffeaae38b90
anthonhertzbie <112811601+anthonhertzbie@users.noreply.github.com>
2022-10-14 10:44:43 +0200

    Small improvement to turn feature; it almost works

--- src/Turn.java
+++ src/Turn.java
@@ -2,11 +2,15 @@ public class Turn {
     public static int turn_number = 0;
 
     public static int playerTurn(int turn_number){
+        int turn = 0;
         if (turn_number % 2 == 0) {
-            System.out.println(" ");
+
+            System.out.println("Player one's turn");
+            turn = 1;
         } else {
-            System.out.println();
+            System.out.println("Player two's turn");
+            turn = 2;
         }
-        return 1;
+        return turn;
     }
 }
