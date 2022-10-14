1e1eae37a3575a0d2f983023577e2c94b5f3bc81
anthonhertzbie <112811601+anthonhertzbie@users.noreply.github.com>
2022-10-14 10:47:28 +0200

    Comments added

--- src/Turn.java
+++ src/Turn.java
@@ -3,6 +3,7 @@ public class Turn {
 
     public static int playerTurn(int turn_number){
         int turn = 0;
+        // Testing for player turn
         if (turn_number % 2 == 0) {
             System.out.println("Player one's turn");
             turn = 1;
@@ -10,6 +11,7 @@ public class Turn {
             System.out.println("Player two's turn");
             turn = 2;
         }
+        //returning the number of the player whose turn it is
         return turn;
     }
 }
