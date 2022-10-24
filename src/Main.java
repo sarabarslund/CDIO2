aab8a81fe125875750e8143e6434ad3dec692efa
anthonhertzbie <112811601+anthonhertzbie@users.noreply.github.com>
2022-10-24 20:15:24 +0200

    Custom GUI Implementation

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         GameController gameController = new GameController();
+        gameController.startBalance();
         gameController.startGame();
     }
 }
