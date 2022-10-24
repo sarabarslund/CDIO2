23551e607c90cea00e4645a6c373bfdd07874f52
CrazyCows <emil.s.simonsen@gmail.com>
2022-10-24 16:30:26 +0200

    Player class made

---
+++ src/Player.java
@@ -0,0 +1,10 @@
+public class Player {
+    private final int player1 = 1, player2 = 2;
+
+    public int getPlayer1() {
+        return player1;
+    }
+    public int getPlayer2() {
+        return player2;
+    }
+}
